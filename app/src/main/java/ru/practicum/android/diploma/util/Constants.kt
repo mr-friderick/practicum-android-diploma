0eb9221dc4c6859b068cb96439602f52e8c90dbd
Evgen1994-McD <mrmarwell@gmail.com>
2025-11-21 17:12:54 +0500

    +Добавлен файл для констант Constants.kt + Добавлена функция для получения вакансии из агрументов  GetVacancyFromArguments.kt ( Будет удобно для использования  при передаче аргумента на детальный фрагмент о вакансии+ в т.ч. при переходе из избранного )

---
+++ app/src/main/java/ru/practicum/android/diploma/util/Constants.kt
@@ -0,0 +1,12 @@
+package ru.practicum.android.diploma.util
+
+/**
+ * Файл для констант в проекте.
+ * Можно использовать для Shared Prefs и Прочих констант.
+ * Хранение констант в одном месте
+ */
+
+object Constants {
+
+    const val VACANCY = "VACANCY"
+}
