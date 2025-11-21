4926401692e6edf033950ca5d7644969561c92a1
Evgen1994-McD <mrmarwell@gmail.com>
2025-11-21 16:52:59 +0500

    + Функция для дебаунса. Можно дебансить поиск, нажатия и так далее

---
+++ app/src/main/java/ru/practicum/android/diploma/util/Debounce.kt
@@ -0,0 +1,46 @@
+package ru.practicum.android.diploma.util
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+import kotlinx.coroutines.*
+
+/**
+ * Одна Extension Функция для Дебаунса всего.
+ * Можно делать дебаунс нажатий, дебаунс поиска.
+ *
+ * @param delayMillis Временной интервал задержки ( пример: перед поиском или перед повторным нажатием на вакансию ).
+ * @param coroutineScope Scope корутин
+ * @param replaceWithLatest Следует ли использовать только последний параметр или нет ( если поиск - то при true
+ * запрос будет сделан на основании последних изменений)
+ * @param action Действие ( функция которую будем выполнять )
+ */
+fun <T> debounce(
+    delayMillis: Long,
+    coroutineScope: CoroutineScope,
+    replaceWithLatest: Boolean = false,
+    action: suspend (T) -> Unit
+): (T) -> Unit {
+    var job: Job? = null
+
+    return { param: T ->
+        coroutineScope.launch {
+            try {
+                // отменим предыдущую Job если надо
+                if (job?.isActive == true && replaceWithLatest) {
+                    job!!.cancel()
+                }
+
+                // Старт новой Job
+                job = launch {
+                    delay(delayMillis)
+                    action(param)
+                }
+            } catch (e: CancellationException) {
+
+            }
+        }
+    }
+}
