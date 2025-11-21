9420e743ec5729b7295cf7abfbcad6cecbd2fb04
pc <asaselok4@gmail.com>
2025-11-21 17:56:09 +0300

    архитектура

--- app/src/main/java/ru/practicum/android/diploma/presentation/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/RootActivity.kt
@@ -7,13 +7,6 @@ class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_root)
-
-        // Пример использования access token для HeadHunter API
-        //networkRequestExample(accessToken = BuildConfig.API_ACCESS_TOKEN)
-    }
-
-    private fun networkRequestExample(accessToken: String) {
-        // ...
     }
 
 }
