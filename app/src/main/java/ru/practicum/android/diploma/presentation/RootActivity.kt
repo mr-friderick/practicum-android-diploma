c37a54633235223aa8da54608664866e959e67d3
pc <asaselok4@gmail.com>
2025-11-21 16:57:37 +0300

    архитектура

--- app/src/main/java/ru/practicum/android/diploma/presentation/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/RootActivity.kt
@@ -1,8 +1,7 @@
-package ru.practicum.android.diploma.ui.root
+package ru.practicum.android.diploma.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
 
 class RootActivity : AppCompatActivity() {
