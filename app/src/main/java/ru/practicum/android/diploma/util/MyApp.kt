d2e48f16b8a5dc9ae7354b51a31a9e9bdab0284f
Pavel Vinogradov <mr.friderick@ya.ru>
2025-11-21 20:33:57 +0500

    Правки detect

--- app/src/main/java/ru/practicum/android/diploma/util/MyApp.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/MyApp.kt
@@ -2,7 +2,7 @@ package ru.practicum.android.diploma.util
 
 import android.app.Application
 
-class MyApp: Application() {
+class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
     }
