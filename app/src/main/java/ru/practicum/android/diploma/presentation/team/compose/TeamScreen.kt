7e4789e0cde6c09e6c3a09200f94ee735a56bc09
Evgen1994-McD <mrmarwell@gmail.com>
2025-11-22 10:23:05 +0500

    ап

--- app/src/main/java/ru/practicum/android/diploma/presentation/team/compose/TeamScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/team/compose/TeamScreen.kt
@@ -25,9 +25,7 @@ import ru.practicum.android.diploma.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TeamScreen(
-
-) {
+fun TeamScreen() {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -47,13 +45,11 @@ fun TeamScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
-
             Box(
                 modifier = Modifier
                     .padding(top = 10.dp)
                     .fillMaxWidth(),
                 contentAlignment = Alignment.Center,
-
                 ) {
                 Text(
                     text = stringResource(R.string.team_screen_head),
@@ -61,19 +57,14 @@ fun TeamScreen(
                     fontSize = 32.sp,
                     fontWeight = FontWeight.Bold,
                     textAlign = TextAlign.Start
-
                 )
             }
-
-
-
 
             Box(
                 modifier = Modifier
                     .padding(top = 32.dp)
                     .fillMaxWidth(),
                 contentAlignment = Alignment.CenterStart,
-
                 ) {
                 Text(
                     text = stringResource(R.string.name1),
@@ -88,7 +79,6 @@ fun TeamScreen(
                     .padding(top = 32.dp)
                     .fillMaxWidth(),
                 contentAlignment = Alignment.CenterStart,
-
                 ) {
                 Text(
                     text = stringResource(R.string.name2),
@@ -103,7 +93,6 @@ fun TeamScreen(
                     .padding(top = 32.dp)
                     .fillMaxWidth(),
                 contentAlignment = Alignment.CenterStart,
-
                 ) {
                 Text(
                     text = stringResource(R.string.name3),
@@ -128,8 +117,6 @@ fun TeamScreen(
                     textAlign = TextAlign.Start
                 )
             }
-
-
         }
     }
 }
