3e25bdbceb3e07b0e4d7ab6e7500e4575835f5cd
Pavel Vinogradov <69008871+mr-friderick@users.noreply.github.com>
2025-11-24 06:44:45 +0500

    GH-5 | Data cлоя, работа с api и sharedPref

---
+++ app/src/main/java/ru/practicum/android/diploma/data/network/HttpCode.kt
@@ -0,0 +1,10 @@
+package ru.practicum.android.diploma.data.network
+
+object HttpCode {
+    const val OK = 200
+    const val NO_AUTH = 403
+    const val NOT_FOUND = 404
+    const val BAD_REQUEST = 400
+    const val INTERNAL_ERROR = 500
+    const val NOT_CONNECTION = -1
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/data/network/Response.kt
@@ -0,0 +1,6 @@
+package ru.practicum.android.diploma.data.network
+
+open class Response {
+    var resultCode = HttpCode.OK
+    var errorMassage = ""
+}
