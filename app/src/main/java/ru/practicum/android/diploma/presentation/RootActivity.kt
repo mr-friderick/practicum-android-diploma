af6ae8f66035e6845a35e2d2d8536de5f0759d4e
Evgen1994-McDgit config --global user.email mrmarwell@gmail.comgit config --listgit config --global user.name Evgen1994-McD <mrmarwell@gmail.com>
2025-11-24 14:07:32 +0500

    bottom_navigation_menu.xml

--- app/src/main/java/ru/practicum/android/diploma/presentation/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/RootActivity.kt
@@ -2,6 +2,7 @@ package ru.practicum.android.diploma.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -20,7 +21,32 @@ class RootActivity : AppCompatActivity() {
         navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         navController = navHostFragment?.navController
-        bottomNavigationView = findViewById()
-        bottomNavigationView.setupWithNavController(navController)
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        navController?.let { bottomNavigationView.setupWithNavController(it) }
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.searchFragment -> navigateAndClearOldFragments(R.id.searchFragment)
+                R.id.favoriteFragment -> navigateAndClearOldFragments(R.id.favoriteFragment)
+                R.id.teamFragment -> navigateAndClearOldFragments(R.id.teamFragment)
+                else -> false
+            }
+        }
+        navController?.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.searchFragment -> bottomNavigationView.isVisible = true
+                R.id.favoriteFragment -> bottomNavigationView.isVisible = true
+                R.id.teamFragment -> bottomNavigationView.isVisible = true
+                else -> bottomNavigationView.isVisible = false
+            }
+        }
+
+    }
+
+    private fun navigateAndClearOldFragments(destinationId: Int): Boolean {
+        // Чистим стек навигации и переходим на указанный пункт
+        navController?.popBackStack()
+        navController?.navigate(destinationId)
+        return true
     }
 }
