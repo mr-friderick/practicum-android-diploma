4e2852af417019200efdda8a9653d8f84f7da121
Pavel Vinogradov <mr.friderick@ya.ru>
2025-11-28 06:25:03 +0500

    Экран Команда

--- app/src/main/java/ru/practicum/android/diploma/presentation/team/compose/TeamScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/team/compose/TeamScreen.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.diploma.presentation.team.compose
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -15,12 +14,10 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import ru.practicum.android.diploma.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -30,7 +27,9 @@ fun TeamScreen() {
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = stringResource(R.string.team))
+                    Text(
+                        text = stringResource(R.string.team)
+                    )
                 }
             )
         }
@@ -39,82 +38,53 @@ fun TeamScreen() {
             modifier = Modifier
                 .padding(horizontal = 16.dp)
                 .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
                 .padding(paddingValues),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top
         ) {
             Box(
                 modifier = Modifier
-                    .padding(top = 10.dp)
+                    .padding(top = 10.dp, bottom = 32.dp)
                     .fillMaxWidth(),
-                contentAlignment = Alignment.Center
+                contentAlignment = Alignment.CenterStart
             ) {
                 Text(
                     text = stringResource(R.string.team_screen_head),
-                    color = Color.Black,
-                    fontSize = 32.sp,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Start
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.displayMedium
                 )
             }
 
-            Box(
-                modifier = Modifier
-                    .padding(top = 32.dp)
-                    .fillMaxWidth(),
-                contentAlignment = Alignment.CenterStart
-            ) {
-                Text(
-                    text = stringResource(R.string.name1),
-                    color = Color.Black,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Start
-                )
-            }
-            Box(
-                modifier = Modifier
-                    .padding(top = 32.dp)
-                    .fillMaxWidth(),
-                contentAlignment = Alignment.CenterStart
-            ) {
-                Text(
-                    text = stringResource(R.string.name2),
-                    color = Color.Black,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Start
-                )
-            }
-            Box(
-                modifier = Modifier
-                    .padding(top = 32.dp)
-                    .fillMaxWidth(),
-                contentAlignment = Alignment.CenterStart
-            ) {
-                Text(
-                    text = stringResource(R.string.name3),
-                    color = Color.Black,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Start
-                )
-            }
-            Box(
-                modifier = Modifier
-                    .padding(top = 32.dp)
-                    .fillMaxWidth(),
-                contentAlignment = Alignment.CenterStart
-            ) {
-                Text(
-                    text = stringResource(R.string.name4),
-                    color = Color.Black,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.Bold,
-                    textAlign = TextAlign.Start
-                )
-            }
+            NameBox(
+                text = stringResource(R.string.name1)
+            )
+            NameBox(
+                text = stringResource(R.string.name2)
+            )
+            NameBox(
+                text = stringResource(R.string.name3)
+            )
+            NameBox(
+                text = stringResource(R.string.name4)
+            )
         }
+    }
+}
+
+@Composable
+fun NameBox(
+    text: String
+) {
+    Box(
+        modifier = Modifier
+            .padding(bottom = 16.dp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Text(
+            text = text,
+            fontWeight = FontWeight.Medium,
+            textAlign = TextAlign.Start,
+        )
     }
 }
