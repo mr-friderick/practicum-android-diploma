4b36d47eed91e53a7c3ca9c57865bf836a45ea37
mr-friderick <mr.friderick@ya.ry>
2025-11-30 12:50:40 +0300

    Актуализация ветки

--- app/src/main/java/ru/practicum/android/diploma/domain/models/VacancySearchState.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/VacancySearchState.kt
@@ -1,7 +1,6 @@
 package ru.practicum.android.diploma.domain.models
 
 sealed interface VacancySearchState {
-    data class Vacancy(val vacancy: VacancyModel) : VacancySearchState
     data class VacancyDetail(val vacancyDetail: VacancyDetailModel) : VacancySearchState
     object NotFound : VacancySearchState
     object NoInternet : VacancySearchState
