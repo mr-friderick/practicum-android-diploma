4c0304d0fc80ca7bf09bde08cf161b9e205b01c5
pc <asaselok4@gmail.com>
2025-11-30 19:02:32 +0300

    экран избранного

--- app/src/main/java/ru/practicum/android/diploma/presentation/favourites/compose/FavoriteScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/favourites/compose/FavoriteScreen.kt
@@ -1,9 +1,13 @@
 package ru.practicum.android.diploma.presentation.favourites.compose
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -13,17 +17,34 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.presentation.theme.AppTheme
+import ru.practicum.android.diploma.presentation.theme.PaddingBase
+
+@Preview
+@Composable
+fun FavoriteScreenPreview() {
+    AppTheme { FavoriteScreen() }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoriteScreen() {
     Scaffold(
+        modifier = Modifier
+            .fillMaxSize(),
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = stringResource(R.string.favorite))
+                    Text(
+                        text = stringResource(R.string.favorite),
+                        style = MaterialTheme.typography.titleLarge
+                    )
                 }
             )
         }
@@ -33,10 +54,55 @@ fun FavoriteScreen() {
                 .fillMaxSize()
                 .background(MaterialTheme.colorScheme.background)
                 .padding(paddingValues),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            // Empty content
+            //использовать VacancyItem для вакансий
+            ListVacanciesEmpty()
         }
+    }
+}
+
+@Preview
+@Composable
+fun NoGetListVacancies() {
+    ImageWithText(
+        imageRes = R.drawable.cat,
+        textRes = R.string.couldnt_get_list_vacancies
+    )
+}
+
+@Preview
+@Composable
+fun ListVacanciesEmpty() {
+    ImageWithText(
+        imageRes = R.drawable.man_with_magnifying,
+        textRes = R.string.list_empty
+    )
+}
+
+@Composable
+private fun ImageWithText(
+    imageRes: Int,
+    textRes: Int
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painterResource(id = imageRes),
+            contentDescription = null,
+            modifier = Modifier.fillMaxWidth(),
+            contentScale = ContentScale.FillWidth
+        )
+        Spacer(modifier = Modifier.height(PaddingBase))
+
+        Text(
+            stringResource(textRes),
+            style = MaterialTheme.typography.titleLarge,
+            textAlign = TextAlign.Center
+        )
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/compose/VacancyDetailScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/vacancy/compose/VacancyDetailScreen.kt
@@ -58,7 +58,9 @@ fun VacancyDetailScreen(onBackClick: () -> Unit) {
         topBar = {
             TopAppBar(
                 title = {
-                    Text(text = stringResource(R.string.vacancy))
+                    Text(
+                        text = stringResource(R.string.vacancy),
+                        style = MaterialTheme.typography.titleLarge)
                 },
                 navigationIcon = {
                     Icon(
