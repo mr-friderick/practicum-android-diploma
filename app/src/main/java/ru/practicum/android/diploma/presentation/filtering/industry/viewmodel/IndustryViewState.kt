d36c010b0f93af5acca992677ce3c430c8c69fbe
mr-friderick <mr.friderick@ya.ry>
2025-12-05 16:14:08 +0300

    Правки detekt

--- app/src/main/java/ru/practicum/android/diploma/presentation/filtering/industry/viewmodel/IndustryViewState.kt
+++ app/src/main/java/ru/practicum/android/diploma/presentation/filtering/industry/viewmodel/IndustryViewState.kt
@@ -2,10 +2,9 @@ package ru.practicum.android.diploma.presentation.filtering.industry.viewmodel
 
 import ru.practicum.android.diploma.domain.models.FilterIndustryModel
 
-sealed interface IndustryViewState{
+sealed interface IndustryViewState {
     object Loading : IndustryViewState
-    data class Industry(
-        val industries: List<FilterIndustryModel>) : IndustryViewState
+    data class Industry(val industries: List<FilterIndustryModel>) : IndustryViewState
     object NoInternet : IndustryViewState
     data class Error(val message: String) : IndustryViewState
 }
