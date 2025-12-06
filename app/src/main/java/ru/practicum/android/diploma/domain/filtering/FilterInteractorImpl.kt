df05b889d3673f4ec57bc3040d1c41ba81eec137
mr-friderick <mr.friderick@ya.ry>
2025-12-06 13:04:13 +0300

    Добавил поиск стран

--- app/src/main/java/ru/practicum/android/diploma/domain/filtering/FilterInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/filtering/FilterInteractorImpl.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.domain.filtering
 
 import kotlinx.coroutines.flow.Flow
+import ru.practicum.android.diploma.domain.models.FilterAreaModel
 import ru.practicum.android.diploma.domain.models.FilterIndustryModel
 import ru.practicum.android.diploma.domain.models.SearchState
 
@@ -10,5 +11,9 @@ class FilterInteractorImpl(
 
     override fun searchIndustries(): Flow<SearchState<List<FilterIndustryModel>>> {
         return repository.searchIndustries()
+    }
+
+    override fun searchCountries(): Flow<SearchState<List<FilterAreaModel>>> {
+        return repository.searchCountries()
     }
 }
